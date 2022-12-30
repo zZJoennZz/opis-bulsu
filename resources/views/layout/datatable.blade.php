bdc27b3286b235d5cf83a74a76b31e829d50973c
roncy12 <60778031+roncy12@users.noreply.github.com>
2022-12-30 14:53:40 +0800

    created_at order by descending

--- resources/views/layout/datatable.blade.php
+++ resources/views/layout/datatable.blade.php
@@ -1,7 +1,15 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.13.1/cr-1.6.1/r-2.4.0/datatables.min.css"/>
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.13.1/cr-1.6.1/r-2.4.0/datatables.min.js"></script>
+
 <script defer>
     $(document).ready(function() {
-        $('#{{ $tableId }}').DataTable();
+        $('#{{ $tableId }}').DataTable(
+            @if(isset($columnId))
+                {   
+                    columnDefs: [ { "targets":[{{$columnId}}], "visible":false, "searchable": false } ],
+                    order: [[ {{$columnId}}, 'desc' ]],       
+                }
+            @endif
+        );
     });
 </script>
