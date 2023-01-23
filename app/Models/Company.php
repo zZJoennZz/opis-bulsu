54178abac12edf4c43f168db49fc8482313eded0
Joenn Aquilino <zzjoennzz@gmail.com>
2023-01-24 05:47:48 +0800

    New: Company model and migration

---
+++ app/Models/Company.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'full_address',
+        'tin',
+        'contact_number',
+        'email_address',
+        'is_delete',
+        'added_by'
+    ];
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'added_by');
+    }
+}
---
+++ database/migrations/2023_01_23_165734_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('full_address')->nullable();
+            $table->string('tin')->nullable();
+            $table->string('contact_number')->nullable();
+            $table->string('email_address')->nullable();
+            $table->boolean('is_delete')->default(0);
+            $table->unsignedBigInteger('added_by');
+            $table->timestamps();
+
+            $table->foreign('added_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
