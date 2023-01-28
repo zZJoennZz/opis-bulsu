af619a60bc76c61ecd270d091a79845aff18a552
Joenn Aquilino <zzjoennzz@gmail.com>
2023-01-28 18:21:01 +0800

    Add: day of week to system time

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,6 +3,7 @@
                 let systemClock = $('#system-clock');
                 function runClock() {
                     systemClock.html(new Date().toLocaleString('en-PH', {
+                        weekday: "long",
                         month: '2-digit',
                         day: '2-digit',
                         year: 'numeric',
