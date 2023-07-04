0e529c5ccedf724a837ba610b3b8209b5601c6da
zZJoennZz <zzjoennzz@gmail.com>
2023-07-05 00:28:12 +0800

    Replaced the dashboard data

--- resources/views/layout/so_dashboard.blade.php
+++ resources/views/layout/so_dashboard.blade.php
@@ -2,82 +2,65 @@
     <h2 class="fw-bold text-uppercase fs-5">Supply Office Dashboard</h2>
 </div>
 <div class="row">
-    <div class="col-12">
+    <div class="col-sm-12 col-md-9">
         <div class="table-responsive">
-            <table class="table table-sm table-hover border-dark caption-top" id="ics-items">
-                <caption class="fw-bold bg-primary text-light ps-1 rounded-top">ICS Items</caption>
-                <thead>
+            <table class="table table-sm table-hover border-primary caption-top" id="5-recent-transactions">
+                <caption class="border border-primary text-primary px-1 py-1 rounded-top">
+                    <span class="text-uppercase">5 recent transactions</span> <a href="{{ route('trans.all') }}" class="btn btn-sm btn-outline-primary float-end">View All</a>
+                </caption>
+                <thead class="text-secondary" style="font-size: 11px;">
                     <tr>
-                        <th>Item Name</th>
-                        <th>Total Cost</th>
-                        <th>Fund Cluster</th>
+                        <th></th>
+                        <th style="width: 50%">Details</th>
+                        <th>Date Acquired</th>
+                        <th>Date Issued</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($ics_inventory as $i)
-                    @foreach ($i->items as $item)
-                    <tr>
-                        <td>
-                            <div class="fw-bold mb-1">
-                                {{ $item->bac_reso_item->quotation->pr_item->ppmp->item_detail->description }}
-                                @if ($i->type === "ICSL")
-                                <span class="badge bg-secondary">Low Value</span>
-                                @else
-                                <span class="badge bg-primary">High Value</span>
-                                @endif
-                            </div>
-                            <div class="text-secondary small"><strong>ICS No.:</strong> {{ $i->number }}</div>
-                            <div class="text-secondary small"><strong>Date Acquired:</strong> {{ date('Y-m-d', strtotime($i->date_acquired)) }}</div>
-                            <div class="text-secondary small"><strong>Issued By:</strong>
-                                @foreach ($i->issuers as $issuer)
-                                {{ $issuer->employee->first_name }} {{ $issuer->employee->middle_name }} {{ $issuer->employee->last_name }}
-                                / <span class="fst-italic">{{ $issuer->employee->position->name }}</span>
-                                @endforeach
-                            </div>
-                        </td>
-                        <td>₱ {{ number_format($item->quantity * $item->unit_price, 2) }}</td>
-                        <td>{{ $item->bac_reso_item->quotation->pr_item->ppmp->source_of_fund->description }}</td>
-                    </tr>
-                    @endforeach
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        <hr />
-        <div class="table-responsive">
-            <table class="table table-sm table-hover border-dark caption-top" id="par-items">
-                <caption class="fw-bold bg-primary text-light ps-1 rounded-top">PAR Items</caption>
-                <thead>
-                    <tr>
-                        <th>Item Name</th>
-                        <th>Total Cost</th>
-                        <th>Fund Cluster</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($par_inventory as $i)
-                    @foreach ($i->items as $item)
-                    <tr>
-                        <td>
-                            <div class="fw-bold mb-1">{{ $item->bac_reso_item->quotation->pr_item->ppmp->item_detail->description }}</div>
-                            <div class="text-secondary small"><strong>PAR No.:</strong> {{ $i->number }}</div>
-                            <div class="text-secondary small"><strong>Date Acquired:</strong> {{ date('Y-m-d', strtotime($i->date_acquired)) }}</div>
-                            <div class="text-secondary small"><strong>Issued By:</strong>
-                                @foreach ($i->issuers as $issuer)
-                                {{ $issuer->employee->first_name }} {{ $issuer->employee->middle_name }} {{ $issuer->employee->last_name }}
-                                / <span class="fst-italic">{{ $issuer->employee->position->name }}</span>
-                                @endforeach
-                            </div>
-                        </td>
-                        <td>₱ {{ number_format($item->quantity * $item->unit_price, 2) }}</td>
-                        <td>{{ $item->bac_reso_item->quotation->pr_item->ppmp->source_of_fund->description }}</td>
-                    </tr>
-                    @endforeach
+                    @foreach ($lastFiveTransactions as $transaction)
+                        <tr>
+                            <td>
+                                <a class="btn btn-primary btn-sm"><em class="bi bi-eye-fill"></em></a>
+                            </td>
+                            <td>
+                                <div>
+                                    @if ($transaction->type === "PAR")
+                                        <span class="badge bg-success">PAR</span>
+                                    @endif
+                                    @if ($transaction->type === "ICSL")
+                                        <span class="badge bg-secondary">ICS <em class="bi bi-caret-down-fill"></em></span>
+                                    @endif
+                                    @if ($transaction->type === "ICSH")
+                                        <span class="badge bg-primary">ICS <em class="bi bi-caret-up-fill"></em></span>
+                                    @endif
+                                    <div class="fw-bold">{{ $transaction->number }}</div>
+                                    <div class="small">
+                                        @php
+                                            $numOfItems = count($transaction->items);
+                                        @endphp
+                                       {{ $numOfItems }} 
+                                       @if ($numOfItems > 1)
+                                           items
+                                        @else
+                                            item
+                                       @endif
+                                    </div>
+                                </div>
+                            </td>
+                            <td>{{ $transaction->date_acquired }}</td>
+                            <td>{{ $transaction->date_issued }}</td>
+                        </tr>
                     @endforeach
                 </tbody>
             </table>
         </div>
     </div>
+    <div class="col-sm-12 col-md-3">
+        <div class="text-primary text-uppercase mb-1">Available Items</div>
+        <div>
+            <div class="text-center fw-bold text-primary" style="font-size: 100px;">{{ $totalUntransferredItems }}</div>
+            <div class="text-center"><a href="{{ route('ics.all') }}" class="btn btn-sm btn-link">View ICS Items</a></div>
+            <div class="text-center"><a href="{{ route('par.all') }}" class="btn btn-sm btn-link">View PAR Items</a></div>
+        </div>
+    </div>
 </div>
-@include('layout/datatable', ['tableId' => 'par-items'])
-@include('layout/datatable', ['tableId' => 'ics-items'])
